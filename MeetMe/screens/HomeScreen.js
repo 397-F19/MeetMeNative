756d50d4d445d3287a805528b0e66624def0e3bc
ahkaneti <aaronkaneti2020@u.northwestern.edu>
2019-11-20 17:04:09 -0600

    Messing around with react-native-calendars

--- MeetMe/screens/HomeScreen.js
+++ MeetMe/screens/HomeScreen.js
@@ -9,55 +9,78 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import { Calendar, CalendarList, Agenda, calendarTheme } from 'react-native-calendars';
 
 import { MonoText } from '../components/StyledText';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
-
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
-          </View>
-
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
-          </Text>
-        </View>
-
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-
+      <Agenda
+        // the list of items that have to be displayed in agenda. If you want to render item as empty date
+        // the value of date key kas to be an empty array []. If there exists no value for date key it is
+        // considered that the date in question is not yet loaded
+        items={{
+          '2019-11-22': [{ text: 'item 1 - any js object' }],
+          '2019-11-23': [{ text: 'item 2 - any js object' }],
+          '2019-11-24': [],
+          '2019-11-25': [{ text: 'item 3 - any js object' }, { text: 'any js object' }]
+        }}
+        // callback that gets called when items for a certain month should be loaded (month became visible)
+        loadItemsForMonth={(month) => { console.log('trigger items loading') }}
+        // callback that fires when the calendar is opened or closed
+        onCalendarToggled={(calendarOpened) => { console.log(calendarOpened) }}
+        // callback that gets called on day press
+        onDayPress={(day) => { console.log('day pressed') }}
+        // callback that gets called when day changes while scrolling agenda list
+        onDayChange={(day) => { console.log('day changed') }}
+        // initially selected day
+        selected={'2019-11-20'}
+        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+        minDate={'2019-11-20'}
+        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+        maxDate={'2019-11-30'}
+        // Max amount of months allowed to scroll to the past. Default = 50
+        // pastScrollRange={50}
+        // Max amount of months allowed to scroll to the future. Default = 50
+        // futureScrollRange={50}
+        // specify how each item should be rendered in agenda
+        renderItem={(item, firstItemInDay) => { return (<View />); }}
+        // specify how each date should be rendered. day can be undefined if the item is not first in that day.
+        renderDay={(day, item) => { return (<View />); }}
+        // specify how empty date content with no items should be rendered
+        renderEmptyDate={() => { return (<View />); }}
+        // specify how agenda knob should look like
+        renderKnob={() => { return (<View />); }}
+        // specify what should be rendered instead of ActivityIndicator
+        renderEmptyData={() => { return (<View />); }}
+        // specify your item comparison function for increased performance
+        rowHasChanged={(r1, r2) => { return r1.text !== r2.text }}
+        // Hide knob button. Default = false
+        hideKnob={false}
+        // By default, agenda dates are marked if they have at least one item, but you can override this if needed
+        markedDates={{
+          '2019-11-20': { selected: true, marked: true },
+          '2019-11-21': { marked: false },
+        }}
+        // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
+        onRefresh={() => console.log('refreshing...')}
+        // Set this true while waiting for new data from a refresh
+        refreshing={false}
+        // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
+        refreshControl={null}
+        // agenda theme
+        theme={{
+          ...calendarTheme,
+          agendaDayTextColor: 'yellow',
+          agendaDayNumColor: 'green',
+          agendaTodayColor: 'red',
+          agendaKnobColor: 'blue'
+        }}
+        // agenda container style
+        style={{}}
+      />
       <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
-        </Text>
-
         <View
           style={[styles.codeHighlightContainer, styles.navigationFilename]}>
           <MonoText style={styles.codeHighlightText}>
