c6dc74bddc294ae8e1008615705debe148ec18d1
ahkaneti <aaronkaneti2020@u.northwestern.edu>
2019-11-20 18:20:44 -0600

    took unnecessary stuff out of home

--- MeetMe/screens/HomeScreen.js
+++ MeetMe/screens/HomeScreen.js
@@ -21,52 +21,6 @@ export default class HomeScreen extends React.Component {
       </View>
     );
   }
-  loadItems(day) {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = this.timeToString(time);
-        if (!this.state.items[strTime]) {
-          this.state.items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 5);
-          for (let j = 0; j < numItems; j++) {
-            this.state.items[strTime].push({
-              name: 'Item for ' + strTime,
-              height: Math.max(50, Math.floor(Math.random() * 150))
-            });
-          }
-        }
-      }
-      //console.log(this.state.items);
-      const newItems = {};
-      Object.keys(this.state.items).forEach(key => { newItems[key] = this.state.items[key]; });
-      this.setState({
-        items: newItems
-      });
-    }, 1000);
-    // console.log(`Load Items for ${day.year}-${day.month}`);
-  }
-
-  renderItem(item) {
-    return (
-      <View style={[styles.item, { height: item.height }]}><Text>{item.name}</Text></View>
-    );
-  }
-
-  renderEmptyDate() {
-    return (
-      <View style={styles.emptyDate}><Text>This is empty date!</Text></View>
-    );
-  }
-
-  rowHasChanged(r1, r2) {
-    return r1.name !== r2.name;
-  }
-
-  timeToString(time) {
-    const date = new Date(time);
-    return date.toISOString().split('T')[0];
-  }
 }
 
 HomeScreen.navigationOptions = {
