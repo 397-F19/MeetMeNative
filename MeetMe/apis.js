317df9c971ce7a664511a967a4bf3bf3a6438955
Terry Tan <tmy1995hflc@gmail.com>
2019-11-20 19:52:15 -0600

    add getUserEvents and createUser functions

--- MeetMe/apis.js
+++ MeetMe/apis.js
@@ -37,3 +37,67 @@ export async function postRequest(route, data = null) {
 		return res.data;
 	}
 }
+
+export async function getUserEvents(firebase, uid) {
+	try {
+		const db = firebase.database();
+		const eventsRef = db.ref(`/data/events`);
+		let eventsSnapshot = await eventsRef.once('value');
+		let events = await Object.values(eventsSnapshot.val());
+		let response = [];
+		console.log(events);
+		for (let i = 0; i < events.length; i += 1) {
+			if (events[i].attendeeUID) {
+				let attendees = await events[i].attendeeUID;
+				for (let a = 0; a < attendees.length; a += 1) {
+					if (attendees[a] === uid) {
+						let usersSnapshot = await db
+							.ref(`/data/users/${events[i].owner}`)
+							.once('value');
+						let userData = usersSnapshot.val();
+						events[i].owner = userData.name;
+						response.push(events[i]);
+						console.log(events[i]);
+					}
+				}
+			}
+		}
+		console.log(response);
+		return response;
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+export async function createUser(firebase, data) {
+	const db = firebase.database();
+	const { uid, name, email, avatar } = data;
+
+	const friendsRef = db.ref(`/data/users/${uid}/friends`);
+	let friendsSnapshot = await friendsRef.once('value');
+	let friendsList = friendsSnapshot.val();
+
+	const eventsRef = db.ref(`/data/users/${uid}/eventsList`);
+	let eventsSnapshot = await eventsRef.once('value');
+	let eventsList = eventsSnapshot.val();
+
+	let postData = {
+		[uid]: {
+			name: name,
+			email: email,
+			avatar: avatar,
+			friends: friendsList || ['placeholder'],
+			eventsList: eventsList || ['placeholder']
+		}
+	};
+	let res = {
+		uid,
+		friendsList
+	};
+	var ref = db.ref('/data/users');
+	await ref.update(postData);
+	console.log(res);
+	let friendsListString = JSON.stringify(res.friendsList);
+	localStorage.setItem('friendsList', friendsListString);
+	return res;
+}
